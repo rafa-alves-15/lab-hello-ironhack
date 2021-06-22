2988685ecf13c702dfe79c0abc96cd01defb18d1
Rafael Alves <rafaelalves.15@outlook.com>
2021-06-21 21:58:00 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./assets/images/ironhack-logo.svg";
+import menu from "./assets/images/menu-top.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <nav className="Nav">
+          <img src={logo} className="App-logo" alt="logo" />
+          <img src={menu} className="Menu-logo" alt="menu" />
+        </nav>
+        <h1>
+          Say hello to<br></br>
+          ReactJS
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You will learn how to use<br></br>
+          the monst popular frontend library,<br></br>
+          and become a super Ninja developer.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=".#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Awesome!
         </a>
       </header>
+      <div>
+      
+
+      </div>
     </div>
   );
 }
